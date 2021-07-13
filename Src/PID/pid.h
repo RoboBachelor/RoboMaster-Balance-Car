5ae5af62bd62c5ab2c1ad362a4f24ee11f6f55c8
xeonz1 <seasoundzhao@outlook.com>
2021-07-13 15:08:23 +0800

    add filter to pid

--- Src/PID/pid.h
+++ Src/PID/pid.h
@@ -2,7 +2,7 @@
   ****************************(C) COPYRIGHT 2016 DJI****************************
   * @file       pid.c/h
   * @brief      pid实现函数，包括初始化，PID计算函数，
-  * @note       
+  * @note
   * @history
   *  Version    Date            Author          Modification
   *  V1.0.0     Dec-26-2018     RM              1. 完成
@@ -17,23 +17,20 @@
 #ifndef PID_H
 #define PID_H
 #include "main.h"
-enum PID_MODE
-{
-    PID_POSITION = 0,
-    PID_DELTA
-};
+enum PID_MODE { PID_POSITION = 0, PID_DELTA };
 
-typedef struct
-{
+typedef struct {
     uint8_t mode;
-    //PID 三参数
+    // PID 三参数
     float Kp;
     float Ki;
     float Kd;
 
-    float max_out;  //最大输出
-    float max_iout; //最大积分输出
+    float max_out;   //最大输出
+    float max_iout;  //最大积分输出
 
+    float derivative_output_filter_coefficient;
+    float proportion_output_filter_coefficient;
     float set;
     float fdb;
 
@@ -41,8 +38,8 @@ typedef struct
     float Pout;
     float Iout;
     float Dout;
-    float Dbuf[3];  //微分项 0最新 1上一次 2上上次
-    float error[3]; //误差项 0最新 1上一次 2上上次
+    float Dbuf[3];   //微分项 0最新 1上一次 2上上次
+    float error[3];  //误差项 0最新 1上一次 2上上次
 
 } PidTypeDef;
 extern void PID_Init(PidTypeDef *pid, uint8_t mode, const float PID[3], float max_out, float max_iout);
